5b40a7fbd460afb47ca66b94188b6de58dab074d
André Aquilina <andre.aquilina@dysismedical.com>
2023-08-11 11:01:47 +0200

    Add zsh installation.

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 cat bashrc.additions >> ~/.bashrc
 
+apt install -y zsh
 # oh-my-zsh & plugins
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~

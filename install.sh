63fc150fee910366810e4afd658e0f7f70906d90
André Aquilina <andre.aquilina@dysismedical.com>
2023-08-11 11:07:54 +0200

    Add change shell command.

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+chsh -s $(which zsh)

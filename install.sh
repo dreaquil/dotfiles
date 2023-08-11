25a7df722f49029a115d8b718f8c13a22b0a4ffa
André Aquilina <andre.aquilina@dysismedical.com>
2023-08-11 10:57:24 +0200

    Remove gitconfig copy.

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 cat bashrc.additions >> ~/.bashrc
 
-cp ./.gitconfig ~
-
 # oh-my-zsh & plugins
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'

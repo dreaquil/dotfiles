7c47b4d0577aaf241a56f4547900ada5b6f4fd17
André Aquilina <andre.aquilina@dysismedical.com>
2023-08-11 11:05:06 +0200

    Remove zshrc copy.

--- install.sh
+++ install.sh
@@ -7,4 +7,3 @@ apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
-cp ./.zshrc ~
